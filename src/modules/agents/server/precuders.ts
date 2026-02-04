afc08662e1984033387731710a60759649e30f42
maxsusladev <maxsusladev@gmail.com>
2026-02-04 16:12:18 +0100

    14: agent page

--- src/modules/agents/server/precuders.ts
+++ src/modules/agents/server/precuders.ts
@@ -5,6 +5,7 @@ import { agentsInsertSchema } from '../schemas';
 import z from 'zod';
 import { eq, getTableColumns, sql, and, ilike, desc, count } from 'drizzle-orm';
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from '@/constants';
+import { TRPCError } from '@trpc/server';
 
 
 export const agentsRouter = createTRPCRouter({
@@ -55,16 +56,25 @@ export const agentsRouter = createTRPCRouter({
                 totalPages,
             }
         }),
-    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-        const [existingAgent] = await db
-            .select({
-                meetingCount: sql<number>`5`,
-                ...getTableColumns(agents)
-            })
-            .from(agents)
-            .where(eq(agents.id, input.id))
-        return existingAgent;
-    }),
+    getOne: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .query(async ({ input, ctx }) => {
+            const [existingAgent] = await db
+                .select({
+                    meetingCount: sql<number>`5`,
+                    ...getTableColumns(agents)
+                })
+                .from(agents)
+                .where(and(
+                    eq(agents.id, input.id),
+                    eq(agents.userId, ctx.auth.user.id)
+                ))
+
+            if (!existingAgent) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+            }
+            return existingAgent;
+        }),
     create: protectedProcedure
         .input(agentsInsertSchema)
         .mutation(async ({ input, ctx }) => {
