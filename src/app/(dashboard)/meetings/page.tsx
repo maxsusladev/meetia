d9b10a63c2cdc5c9ca141c290820e5fa56a408ea
maxsusladev <maxsusladev@gmail.com>
2026-02-05 17:48:18 +0100

    16: meeting-setup

---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,34 @@
+
+import { getQueryClient, trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import { ErrorBoundary } from "react-error-boundary"
+import { Suspense } from "react"
+
+import {
+    MeetingsIdViewError,
+    MeetingsIdViewLoading,
+    MeetingsView
+} from "@/modules/meetings/ui/views/meetings-view"
+
+const Page = () => {
+
+    const queryClient = getQueryClient()
+    void queryClient.prefetchQuery(
+        trpc.meetings.getMany.queryOptions({})
+    )
+
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<MeetingsIdViewLoading />}>
+                <ErrorBoundary fallback={<MeetingsIdViewError />}>
+                    <MeetingsView />
+                </ErrorBoundary>
+            </Suspense>
+        </HydrationBoundary>
+    )
+}
+
+export default Page
+
+
---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { ErrorState } from "@/components/error-state"
+import { LoadingState } from "@/components/loading-state"
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query"
+
+
+export const MeetingsView = () => {
+    const trpc = useTRPC()
+
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+
+    return (
+        <div>{JSON.stringify(data)}</div>
+    )
+}
+
+export const MeetingsIdViewLoading = () => {
+    return (
+        <LoadingState title="Loading Meetings" description="This nay take a few seconds..." />
+    )
+}
+
+export const MeetingsIdViewError = () => {
+    return (
+        <ErrorState title="Error loading Meetings" description="Please try Meetings later" />
+    )
+}
