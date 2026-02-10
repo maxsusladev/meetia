af059ad26bca9e9efc91ed304725b7b80913962f
maxsusladev <maxsusladev@gmail.com>
2026-02-10 22:20:26 +0100

    20: meeting change

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,8 +1,46 @@
-const Page = () => {
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { MeetingIdView, MeetingIdViewError, MeetingIdViewLoading } from "@/modules/meetings/ui/views/meeting-id-view";
 
+
+
+
+interface Props {
+    params: Promise<{
+        meetingId: string
+    }>;
+}
+
+
+const Page = async ({ params }: Props) => {
+
+    const { meetingId } = await params;
+    const sessison = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!sessison) {
+        redirect("/sign-in")
+    }
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.meetings.getOne.queryOptions({ id: meetingId })
+    )
 
     return (
-        <div>Meeting Id page</div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<MeetingIdViewLoading />}>
+                <ErrorBoundary fallback={<MeetingIdViewError />}>
+                    <div >
+                        <MeetingIdView meetingId={meetingId} />
+                    </div>
+                </ErrorBoundary>
+            </Suspense>
+        </HydrationBoundary>
     )
 }
 
