7793e4c8e122c64213f1947814b4e48e19d233a3
maxsusladev <maxsusladev@gmail.com>
2026-03-06 13:16:30 +0100

    28: bugs

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -82,8 +82,6 @@ export const MeetingIdView = ({ meetingId }: Props) => {
                 {isUpcoming &&
                     <UpcomingState
                         meetingId={meetingId}
-                        onCancelMeeting={() => { }}
-                        isCancelling={false}
                     />}
             </div>
 
